19c2ef1114ad03f01ec2aa0ce89b3e2fe25bda20
hakou <hakou.silem@gmail.com>
2026-03-11 10:44:00 +0100

    linked the java with xml

--- app/src/main/java/com/example/g3/MainActivity.java
+++ app/src/main/java/com/example/g3/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.g3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText name, password;
+    TextView reset;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        name = findViewById(R.id.et_name);
+        password = findViewById(R.id.et_pass);
+
+
+        reset = findViewById(R.id.tv_reset);
+
+        login = findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = name.getText().toString();
+                String p = password.getText().toString();
+                if(n.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please fill the name field", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(p.isEmpty()){
+                        Toast.makeText(MainActivity.this, "Please fill the password field", Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        if(n.equals("SI") && p.equals("1234")){
+                            Intent intent = new Intent(MainActivity.this, Welcome.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this, "name or password is wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+
+
+            }
         });
+
+
+
     }
 }
